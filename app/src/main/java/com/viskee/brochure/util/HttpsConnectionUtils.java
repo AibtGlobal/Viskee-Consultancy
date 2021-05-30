da11e9101e2f21d5c4a7949ee37cdccaecbd20a7
AibtGlobal <84275727+AibtGlobal@users.noreply.github.com>
2021-05-30 23:13:18 +0200

    handle https connection

--- app/src/main/java/com/viskee/brochure/util/HttpsConnectionUtils.java
+++ app/src/main/java/com/viskee/brochure/util/HttpsConnectionUtils.java
@@ -8,13 +8,16 @@ import com.viskee.brochure.R;
 import java.io.InputStream;
 import java.security.KeyStore;
 import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
 
 public class HttpsConnectionUtils {
 
@@ -24,7 +27,7 @@ public class HttpsConnectionUtils {
             CertificateFactory cf = CertificateFactory.getInstance("X.509");
             // From github raw page: https://raw.githubusercontent.com/AibtGlobal/Viskee-Consultancy-Configuration/master/configuration/AIBT.json
             // To note that this cert is different from the one of default github page
-            InputStream caInput = context.getResources().openRawResource(R.raw.github);
+            InputStream caInput = context.getResources().openRawResource(R.raw.certificate);
             Certificate ca;
             try {
                 ca = cf.generateCertificate(caInput);
@@ -46,12 +49,39 @@ public class HttpsConnectionUtils {
 
             // Create an SSLContext that uses our TrustManager
             SSLContext sslContext = SSLContext.getInstance("TLS");
-            sslContext.init(null, tmf.getTrustManagers(), null);
+            sslContext.init(null, getWrappedTrustManagers(tmf.getTrustManagers()), null);
 
             return sslContext.getSocketFactory();
         } catch (Exception e) {
             Log.e(HttpsConnectionUtils.class.getSimpleName(), e.getMessage());
             return HttpsURLConnection.getDefaultSSLSocketFactory();
         }
+    }
+
+    private static TrustManager[] getWrappedTrustManagers(TrustManager[] trustManagers) {
+        final X509TrustManager originalTrustManager = (X509TrustManager) trustManagers[0];
+        return new TrustManager[]{
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return originalTrustManager.getAcceptedIssuers();
+                    }
+
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                        try {
+                            originalTrustManager.checkClientTrusted(certs, authType);
+                        } catch (CertificateException e) {
+                            Log.e(HttpsConnectionUtils.class.getSimpleName(), e.getMessage());
+                        }
+                    }
+
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                        try {
+                            originalTrustManager.checkServerTrusted(certs, authType);
+                        } catch (CertificateException e) {
+                            Log.e(HttpsConnectionUtils.class.getSimpleName(), e.getMessage());
+                        }
+                    }
+                }
+        };
     }
 }
