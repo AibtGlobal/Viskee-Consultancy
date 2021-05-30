db6fafa0c48507de99a317cca3ba45ab66f498fb
AibtGlobal <84275727+AibtGlobal@users.noreply.github.com>
2021-05-30 17:23:49 +0200

    Revert "handle certificate exception"
    This reverts commit 9532c65d6ba379108a92eee42e468fd9dcab7be4.

--- app/src/main/java/com/viskee/brochure/util/HttpsConnectionUtils.java
+++ app/src/main/java/com/viskee/brochure/util/HttpsConnectionUtils.java
@@ -40,7 +40,6 @@ public class HttpsConnectionUtils {
 
             return sslContext.getSocketFactory();
         } catch (Exception e) {
-            e.printStackTrace();
             return HttpsURLConnection.getDefaultSSLSocketFactory();
         }
     }
@@ -53,12 +52,18 @@ public class HttpsConnectionUtils {
                         return originalTrustManager.getAcceptedIssuers();
                     }
 
-                    public void checkClientTrusted(X509Certificate[] certs, String authType) throws CertificateException {
-                        originalTrustManager.checkClientTrusted(certs, authType);
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                        try {
+                            originalTrustManager.checkClientTrusted(certs, authType);
+                        } catch (CertificateException ignored) {
+                        }
                     }
 
-                    public void checkServerTrusted(X509Certificate[] certs, String authType) throws CertificateException {
-                        originalTrustManager.checkServerTrusted(certs, authType);
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                        try {
+                            originalTrustManager.checkServerTrusted(certs, authType);
+                        } catch (CertificateException ignored) {
+                        }
                     }
                 }
         };
